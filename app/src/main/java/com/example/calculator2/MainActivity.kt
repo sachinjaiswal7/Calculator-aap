f3de88e1ab63760bfbd80b52c5b5cfb1eba4c94a
sachin jaiswal <sj62513@gmail.com>
2021-11-26 19:35:31 +0530

    v1.0.2

--- app/src/main/java/com/example/calculator2/MainActivity.kt
+++ app/src/main/java/com/example/calculator2/MainActivity.kt
@@ -3,19 +3,17 @@ package com.example.calculator2
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.TextView
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-     var   numberchoose: String =findViewById<RadioGroup>(R.id.rdgroup1).checkedRadioButtonId.toString()
 
-     var  signchoose: String = findViewById<RadioGroup>(R.id.rdgroup2).checkedRadioButtonId.toString()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        numberchoose = findViewById<RadioGroup>(R.id.rdgroup1).checkedRadioButtonId.toString()
-        signchoose = findViewById<RadioGroup>(R.id.rdgroup2).checkedRadioButtonId.toString()
         val one = findViewById<TextView>(R.id.one)
         val two = findViewById<TextView>(R.id.two)
         val three = findViewById<TextView>(R.id.three)
@@ -54,7 +52,7 @@ class MainActivity : AppCompatActivity() {
         eight.setOnClickListener { input("8") }
         nine.setOnClickListener { input("9") }
         back.setOnClickListener {
-            if(numberchoose.equals("number1")){
+            if(findViewById<RadioButton>(R.id.number1).isChecked){
                 if(num1.text.isNotEmpty()){
                     num1.text = num1.text.substring(0,num1.text.length-1)
                 }
@@ -77,11 +75,20 @@ class MainActivity : AppCompatActivity() {
                     val str = num1.text.toString()
                     val n1 = str.toDouble()
                     val n2 = num2.text.toString().toDouble()
-                    when(signchoose){
-                        "product"-> ans.text = (n1*n2).toString()
-                        "addition"-> ans.text = (n1+n2).toString()
-                        "division"-> ans.text = (n1/n2).toString()
-                        "substract" -> ans.text = (n1-n2).toString()
+
+                    when {
+                        findViewById<RadioButton>(R.id.addition).isChecked -> {
+                            ans.text = (n1+n2).toString()
+                        }
+                        findViewById<RadioButton>(R.id.product).isChecked -> {
+                            ans.text = (n1*n2).toString()
+                        }
+                        findViewById<RadioButton>(R.id.substract).isChecked -> {
+                            ans.text = (n1-n2).toString()
+                        }
+                        else -> {
+                            ans.text = (n1/n2).toString()
+                        }
                     }
 
                 }
@@ -101,30 +108,21 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    @SuppressLint("SetTextI18n")
     private fun input(s:String?){
-       if(numberchoose == "number1"){
+       if(findViewById<RadioButton>(R.id.number1).isChecked){
            val num1 = findViewById<TextView>(R.id.tv1)
-           num1.append(s)
+           num1.text =num1.text.toString()+s
        }
-        else if(numberchoose == "number2"){
+        else {
             val num2 = findViewById<TextView>(R.id.tv2)
-           num2.append(s)
-       }
-        else{
-            message(this,numberchoose)
+           num2.text = num2.text.toString()+s
        }
 
-    }
-
-    fun numberchooser(view: android.view.View) {
-        val rdgroup = findViewById<RadioGroup>(R.id.rdgroup1)
-        numberchoose = rdgroup.checkedRadioButtonId.toString()
 
     }
 
-    fun signchooser(view: android.view.View) {
-        signchoose = findViewById<RadioGroup>(R.id.rdgroup2).checkedRadioButtonId.toString()
-    }
+
+
+
 
 }
